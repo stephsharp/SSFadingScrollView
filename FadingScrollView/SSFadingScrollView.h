0046793e8ea1c21ec682a09528cd16dd55600012
Julien Goudet <julien.goudet@worldline.com>
2015-06-26 15:41:16 +0200

    [IMP] Improve by adding some initializers

--- FadingScrollView/SSFadingScrollView.h
+++ FadingScrollView/SSFadingScrollView.h
@@ -1,10 +1,52 @@
 //
-//  FadingScrollView.h
-//  Created by Stephanie Sharp on 1/06/13.
+//  SSFadingScrollView.h
+//
+//  Created by julien goudet on 26/06/2015.
+//  Copyright (c) 2015 lbp. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface SSFadingScrollView : UIScrollView
+
+/**
+ *  Call this main init method to put default top and bottom fade with default effect
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)init;
+
+/**
+ *  Call this method to init a UIScrollView with a default fade only at the Top
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)initWithTopFade;
+
+/**
+ *  Call this method to init a UIScrollView with a given percentage of fade, only at the Top
+ *
+ *  @param percent the percent of the scrollview which will be faded at the top
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)initWithTopFade:(float)percent;
+
+/**
+ *  Call this method to init a UIScrollView with a default fade only at the Bottom
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)initWithBottomFade;
+
+/**
+ *  Call this method to init a UIScrollView with a given percentage of fade, only at the Bottom
+ *
+ *  @param percent the percent of the scrollview which will be faded at the Bottom
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)initWithBottomFade:(float)percent;
+
 
 @end
--- FadingScrollView/SSFadingScrollView.m
+++ FadingScrollView/SSFadingScrollView.m
@@ -1,50 +1,174 @@
 //
-//  FadingScrollView.m
-//  Created by Stephanie Sharp on 1/06/13.
+//  SSFadingScrollView.m
+//
+//  Created by julien goudet on 26/06/2015.
+//  Copyright (c) 2015 lbp. All rights reserved.
 //
 
 #import "SSFadingScrollView.h"
 #import <QuartzCore/QuartzCore.h>
 
-#define kScrollBarWidth 7
-#define kFadePercentage 0.1
+
+static float const fadeDefault = 0.10;
+
+@interface SSFadingScrollView () {
+    float fadePercentage;
+    BOOL topFade;
+    BOOL bottomFade;
+}
+
+@end
 
 @implementation SSFadingScrollView
 
-- (void)layoutSubviews
-{
+/**
+ *  Call this main init method to put default top and bottom fade with default effect
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)init {
+    self = [super init];
+    if (self) {
+        fadePercentage = -1;
+        topFade = YES;
+        bottomFade = YES;
+    }
+    return self;
+}
+
+/**
+ *  Call this method to init a UIScrollView with a default fade only at the Top
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)initWithTopFade {
+    self = [super init];
+    if (self) {
+        fadePercentage = -1;
+        topFade = YES;
+        bottomFade = NO;
+    }
+    return self;
+}
+
+/**
+ *  Call this method to init a UIScrollView with a given percentage of fade, only at the Top
+ *
+ *  @param percent the percent of the scrollview which will be faded at the top
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)initWithTopFade:(float)percent {
+    self = [super init];
+    if (self) {
+        fadePercentage = (percent > 0) ? percent : -1;
+        topFade = YES;
+        bottomFade = NO;
+    }
+    return self;
+}
+
+/**
+ *  Call this method to init a UIScrollView with a default fade only at the Bottom
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)initWithBottomFade {
+    self = [super init];
+    if (self) {
+        fadePercentage = -1;
+        topFade = NO;
+        bottomFade = YES;
+    }
+    return self;
+}
+
+/**
+ *  Call this method to init a UIScrollView with a given percentage of fade, only at the Bottom
+ *
+ *  @param percent the percent of the scrollview which will be faded at the Bottom
+ *
+ *  @return the faded UISCrollView
+ */
+- (id)initWithBottomFade:(float)percent {
+    self = [super init];
+    if (self) {
+        fadePercentage = (percent > 0) ? percent : -1;
+        topFade = NO;
+        bottomFade = YES;
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
     [super layoutSubviews];
-    
-    NSObject * transparent = (NSObject *) [[UIColor colorWithWhite:0 alpha:0] CGColor];
-    NSObject * opaque = (NSObject *) [[UIColor colorWithWhite:0 alpha:1] CGColor];
-    
-    CALayer * maskLayer = [CALayer layer];
+
+    NSObject *transparent = (NSObject *)[[UIColor colorWithWhite:0 alpha:0] CGColor];
+    NSObject *opaque = (NSObject *)[[UIColor colorWithWhite:0 alpha:1] CGColor];
+
+    CALayer *maskLayer = [CALayer layer];
     maskLayer.frame = self.bounds;
-    
-    CALayer * scrollGutterLayer = [CALayer layer];
-    scrollGutterLayer.frame = CGRectMake(self.bounds.size.width - kScrollBarWidth, 0,
-                                         kScrollBarWidth, self.bounds.size.height);
-    scrollGutterLayer.backgroundColor = (__bridge CGColorRef)(opaque);
-    
-    CAGradientLayer * gradientLayer = [CAGradientLayer layer];
+
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
     gradientLayer.frame = CGRectMake(self.bounds.origin.x, 0,
                                      self.bounds.size.width, self.bounds.size.height);
-    
-    gradientLayer.colors = [NSArray arrayWithObjects: transparent, opaque,
-                            opaque, transparent, nil];
-    
-    // Set percentage of scrollview that fades at top & bottom
-    gradientLayer.locations = [NSArray arrayWithObjects:
-                               [NSNumber numberWithFloat:0],
-                               [NSNumber numberWithFloat:kFadePercentage],
-                               [NSNumber numberWithFloat:1.0 - kFadePercentage],
-                               [NSNumber numberWithFloat:1], nil];
-    
-    gradientLayer.startPoint=CGPointMake(0.5, 0);
-    gradientLayer.endPoint=CGPointMake(0.5, 1);
-    
+
+
+
+    if (bottomFade) {
+        if (topFade) {
+            gradientLayer.colors = [NSArray arrayWithObjects:
+                                    transparent,
+                                    opaque,
+                                    opaque,
+                                    transparent,
+                                    nil];
+            gradientLayer.locations = [NSArray arrayWithObjects:
+                                       [NSNumber numberWithFloat:0],
+                                       [NSNumber numberWithFloat:((fadePercentage == -1) ? fadeDefault : fadePercentage)],
+                                       [NSNumber numberWithFloat:1.0 - ((fadePercentage == -1) ? fadeDefault : fadePercentage)],
+                                       [NSNumber numberWithFloat:1],
+                                       nil];
+        }
+        else {
+            gradientLayer.colors = [NSArray arrayWithObjects:
+                                    opaque,
+                                    transparent,
+                                    nil];
+            gradientLayer.locations = [NSArray arrayWithObjects:
+                                       [NSNumber numberWithFloat:1.0 - ((fadePercentage == -1) ? fadeDefault : fadePercentage)],
+                                       [NSNumber numberWithFloat:1],
+                                       nil];
+        }
+    }
+    else {
+        if (topFade) {
+            gradientLayer.colors = [NSArray arrayWithObjects:
+                                    transparent,
+                                    opaque,
+                                    nil];
+            gradientLayer.locations = [NSArray arrayWithObjects:
+                                       [NSNumber numberWithFloat:0],
+                                       [NSNumber numberWithFloat:((fadePercentage == -1) ? fadeDefault : fadePercentage)],
+                                       nil];
+        }
+        else {
+            gradientLayer.colors = [NSArray arrayWithObjects:
+                                    transparent,
+                                    opaque,
+                                    opaque,
+                                    transparent,
+                                    nil];
+            gradientLayer.locations = [NSArray arrayWithObjects:
+                                       [NSNumber numberWithFloat:0],
+                                       [NSNumber numberWithFloat:((fadePercentage == -1) ? fadeDefault : fadePercentage)],
+                                       [NSNumber numberWithFloat:1.0 - ((fadePercentage == -1) ? fadeDefault : fadePercentage)],
+                                       [NSNumber numberWithFloat:1],
+                                       nil];
+        }
+    }
+
     [maskLayer addSublayer:gradientLayer];
-    [maskLayer addSublayer:scrollGutterLayer];
     self.layer.mask = maskLayer;
 }
 
