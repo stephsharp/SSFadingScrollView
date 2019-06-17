e8d7e6078adb59ab893a6d77389a15ee9d246c8c
Bartosz Dolewski <ext.bartosz.a.dolewski@tieto.com>
2019-06-17 14:00:13 +0200

    SSFadingScrollView rewriten in Swift

--- SSFadingScrollView.podspec
+++ SSFadingScrollView.podspec
@@ -1,11 +1,11 @@
 Pod::Spec.new do |s|
   s.name         = "SSFadingScrollView"
-  s.version      = "1.1.0"
+  s.version      = "2.0.0"
   s.summary      = "A UIScrollView subclass that fades the leading and/or trailing edges of a scroll view to transparent"
   s.homepage     = "https://github.com/stephsharp/SSFadingScrollView/"
   s.license      = { :type => "MIT", :file => "LICENSE" }
   s.author       = "Stephanie Sharp"
-  s.platform     = :ios, "8.0"
+  s.platform     = :ios, "10.3"
   s.source       = { :git => "https://github.com/stephsharp/SSFadingScrollView.git", :tag => "v#{s.version}" }
   s.source_files = "SSFadingScrollView"
   s.public_header_files = [ "SSFadingScrollView/SSFadingScrollView.h" ]
---
+++ SSFadingScrollView/SSFadingScrollView.swift
@@ -0,0 +1,338 @@
+//
+//  SSFadingScrollView.swift
+//  SSFadingScrollView
+//
+//  Created by Bartosz Dolewski on 16/06/2019.
+//  Copyright © 2019 Stephanie Sharp. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+@objc enum FadeAxis: Int {
+    case vertical = 0
+    case horizontal = 1
+}
+
+@objc enum FadeEdges: Int {
+    case FadeEdgesTopAndBottom
+    case FadeEdgesTop
+    case FadeEdgesBottom
+}
+
+private let SSDefaultFadeSize: CGFloat = 30.0
+private let SSDefaultFadeDuration: CFTimeInterval = 0.3
+
+@IBDesignable
+class SSFadingScrollView: UIScrollView {
+    /**
+     * Fade leading edge of fade axis. Default is YES.
+     */
+    @IBInspectable var fadeLeadingEdge = false
+    /**
+     * Fade trailing edge of fade axis. Default is YES.
+     */
+    @IBInspectable var fadeTrailingEdge = false
+    /**
+     * Size of gradient. Default is 30.
+     */
+    @IBInspectable var fadeSize: CGFloat = 0.0
+    /**
+     * Duration of fade in & out. Default is 0.3 seconds.
+     */
+    @IBInspectable var fadeDuration = 0.0
+    /**
+     * Default is YES. Scroll bars are masked so they don't fade with the scroll view content.
+     * Set to NO to fade out the scroll bars along with the content.
+     */
+    @IBInspectable var maskScrollBars = false
+    
+    @available(*, unavailable, message: "This property is reserved for Interface Builder. Use 'fadeAxis' instead.")
+    @IBInspectable var axisIB: FadeAxis.RawValue {
+        set { fadeAxis = FadeAxis(rawValue: newValue) ?? .vertical }
+        get { return fadeAxis.rawValue }
+    }
+    
+    var fadeAxis: FadeAxis = .vertical
+    
+    private lazy var maskLayer: CALayer = {
+        return setupMaskLayer()
+    }()
+    
+    private lazy var verticalScrollBarLayer: CALayer = {
+        return setupVerticalScrollBarLayer()
+    }()
+    
+    private lazy var horizontalScrollBarLayer: CALayer = {
+        return setupHorizontalScrollBarLayer()
+    }()
+    
+    private lazy var gradientLayer: CAGradientLayer = {
+        return setupGradientLayer()
+    }()
+    
+    private var verticalScrollBar: UIImageView?
+    private var horizontalScrollBar: UIImageView?
+    
+    private var verticalScrollObservation: NSKeyValueObservation?
+    private var horizontalScrollObservation: NSKeyValueObservation?
+    
+    // MARK: - Initializers
+    init(fadeSize: CGFloat, axis fadeAxis: FadeAxis) {
+        super.init(frame: CGRect.zero)
+        setDefaults()
+        self.fadeSize = fadeSize
+        self.fadeAxis = fadeAxis
+        
+        verticalScrollObservation = verticalScrollBar?.observe(\.alpha) { [weak self] object, change in
+            self?.layoutSubviews()
+        }
+        
+        horizontalScrollObservation = horizontalScrollBar?.observe(\.alpha) { [weak self] object, change in
+            self?.layoutSubviews()
+        }
+    }
+    
+    convenience init(fadeSize: CGFloat) {
+        self.init(fadeSize: fadeSize, axis: .vertical)
+    }
+    
+    convenience init() {
+        self.init(fadeSize: SSDefaultFadeSize)
+    }
+    
+    convenience override init(frame: CGRect) {
+        self.init()
+        setDefaults()
+        self.frame = frame
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setDefaults()
+    }
+    
+    func setDefaults() {
+        fadeAxis = .vertical
+        fadeLeadingEdge = true
+        fadeTrailingEdge = true
+        fadeSize = SSDefaultFadeSize
+        fadeDuration = SSDefaultFadeDuration
+        maskScrollBars = true
+    }
+}
+
+extension SSFadingScrollView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateMask()
+    }
+    
+    func setupMaskLayer() -> CALayer {
+        let maskLayer = CALayer()
+        maskLayer.frame = bounds
+        
+        gradientLayer = setupGradientLayer()
+        maskLayer.addSublayer(gradientLayer)
+        
+        if maskScrollBars {
+            maskLayer.addSublayer(verticalScrollBarLayer)
+            maskLayer.addSublayer(horizontalScrollBarLayer)
+        }
+        
+        layer.mask = maskLayer
+        
+        return maskLayer
+    }
+    
+    func setupVerticalScrollBarLayer() -> CALayer {
+        findScrollBars()
+        return scrollBar(with: verticalScrollBar?.frame ?? .zero)
+    }
+    
+    func setupHorizontalScrollBarLayer() -> CALayer {
+        findScrollBars()
+        return scrollBar(with: horizontalScrollBar?.frame ?? .zero)
+    }
+    
+    func updateMask() {
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        maskLayer.frame = bounds
+        CATransaction.commit()
+        
+        updateGradients()
+        updateScrollBarMasks()
+    }
+    
+    func updateGradients() {
+        gradientLayer.frame = maskLayer.bounds
+        let contentOffset = Int(roundf(Float(isVertical ? self.contentOffset.y : self.contentOffset.x)))
+        
+        if fadeLeadingEdge {
+            if !leadingGradientIsHidden && contentOffset <= 0 {
+                animateLeadingGradient(to: opaqueColor) // fade out
+            } else if leadingGradientIsHidden && contentOffset > 0 {
+                animateLeadingGradient(to: transparentColor) // fade in
+            }
+        }
+        if fadeTrailingEdge {
+            var maxContentOffset: Int
+            if isVertical {
+                maxContentOffset = Int(roundf(Float(contentSize.height - bounds.height)))
+            } else {
+                maxContentOffset = Int(roundf(Float(contentSize.width - bounds.width)))
+            }
+            
+            if !trailingGradientIsHidden && contentOffset >= maxContentOffset {
+                animateTrailingGradient(to: opaqueColor)
+            } else if trailingGradientIsHidden && contentOffset < maxContentOffset {
+                animateTrailingGradient(to: transparentColor)
+            }
+        }
+    }
+    
+    func setupGradientLayer() -> CAGradientLayer {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.bounds
+        gradientLayer.frame.origin = .zero
+        
+        var colors: [CGColor] = []
+        var locations: [NSNumber] = []
+        
+        let fadePercentage = percentageForFadeSize
+        
+        if fadeLeadingEdge {
+            colors = [transparentColor, opaqueColor]
+            locations = [NSNumber(value: 0), NSNumber(value: Float(fadePercentage))]
+        }
+        if fadeTrailingEdge {
+            colors = colors + [opaqueColor, transparentColor]
+            locations = locations + [NSNumber(value: Float(1.0 - fadePercentage)), NSNumber(value: 1)]
+        }
+        
+        gradientLayer.colors = colors
+        gradientLayer.locations = locations
+        
+        gradientLayer.startPoint = isVertical ? CGPoint(x: 0.5, y: 0.0) : CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = isVertical ? CGPoint(x: 0.5, y: 1.0) : CGPoint(x: 1.0, y: 0.5)
+        
+        return gradientLayer
+    }
+    
+    func updateScrollBarMasks() {
+        guard maskScrollBars else { return }
+        guard let verticalScrollBar = verticalScrollBar,
+            let horizontalScrollBar = horizontalScrollBar else { return }
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        
+        let verticalScrollBarFrame = layer.convert(verticalScrollBar.frame, to: maskLayer)
+        verticalScrollBarLayer.frame = verticalScrollBarFrame
+        verticalScrollBarLayer.opacity = Float(verticalScrollBar.alpha)
+        
+        let horizontalScrollBarFrame = layer.convert(horizontalScrollBar.frame, to: maskLayer)
+        horizontalScrollBarLayer.frame = horizontalScrollBarFrame
+        horizontalScrollBarLayer.opacity = Float(horizontalScrollBar.alpha)
+        
+        CATransaction.commit()
+    }
+    
+    func scrollBar(with frame: CGRect) -> CALayer {
+        let scrollBarLayer = CALayer()
+        scrollBarLayer.backgroundColor = opaqueColor
+        scrollBarLayer.frame = frame
+        
+        return scrollBarLayer
+    }
+    
+    fileprivate func isScrollBar(_ dimension: CGFloat) -> Bool {
+        return dimension == 3.5 || dimension == 2.5 || ((dimension) < 2.4 && (dimension ) > 2.3)
+    }
+    
+    func findScrollBars() {
+        subviews
+            .filter { ($0 is UIImageView) && $0.tag == 0 }
+            .compactMap { $0 as? UIImageView }
+            .forEach { imageView in
+                if isScrollBar(imageView.frame.size.width) {
+                    verticalScrollBar = imageView
+                } else if isScrollBar(imageView.frame.size.height) {
+                    horizontalScrollBar = imageView
+                }
+        }
+    }
+}
+
+// MARK: - Properties
+extension SSFadingScrollView {
+    var isVertical: Bool {
+        return fadeAxis == .vertical
+    }
+    
+    var leadingGradientIsHidden: Bool {
+        let firstColor = gradientLayer.colors?.first as! CGColor
+        return firstColor == opaqueColor
+    }
+    
+    var trailingGradientIsHidden: Bool {
+        let lastColor = gradientLayer.colors?.last as! CGColor
+        return lastColor == opaqueColor
+    }
+    
+    var opaqueColor: CGColor {
+        return UIColor.black.cgColor
+    }
+    
+    var transparentColor: CGColor {
+        return UIColor.clear.cgColor
+    }
+    
+    var percentageForFadeSize: CGFloat {
+        let size = isVertical ? bounds.height : bounds.width
+        
+        if size <= 0 {
+            return 0
+        }
+        
+        let maxFadePercentage: CGFloat = (fadeLeadingEdge && fadeTrailingEdge) ? 0.5 : 1.0
+        return CGFloat(fminf(Float(fadeSize / size), Float(maxFadePercentage)))
+    }
+}
+
+// MARK: - Gradient animation
+extension SSFadingScrollView {
+    func animateLeadingGradient(to color: CGColor) {
+        let colours = colorsArrayByReplacingObject(at: 0, with: color)
+        animateGradientColours(colours)
+    }
+    
+    func animateTrailingGradient(to color: CGColor) {
+        let lastIndex = gradientLayer.colors!.count - 1
+        let colours = colorsArrayByReplacingObject(at: lastIndex, with: color)
+        animateGradientColours(colours)
+    }
+    
+    func colorsArrayByReplacingObject(at index: Int, with color: CGColor) -> [CGColor] {
+        var mutableColours = gradientLayer.colors as! [CGColor]
+        mutableColours[index] = color
+        
+        return mutableColours
+    }
+    
+    func animateGradientColours(_ colours: [Any]?) {
+        let animation = CABasicAnimation(keyPath: #keyPath(CAGradientLayer.colors))
+        animation.fromValue = (gradientLayer.presentation())?.colors
+        animation.toValue = colours
+        animation.duration = fadeDuration
+        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        gradientLayer.colors = colours // update the model value
+        CATransaction.commit()
+        
+        gradientLayer.add(animation, forKey: "animateGradient")
+    }
+}
